ce9ed4c76d282b25870cf3d0f147e6170afa4c14
Robert Harris <rlh9@rice.edu>
2020-02-12 19:27:37 -0600

    Assumed that DOM manipulation is functioning as designed, will have to check later when time is appropriate

--- script.js
+++ script.js
@@ -9,14 +9,39 @@ document.body.style.transform = scale;     // General
 // Variable define
 var userinput = [];
 var display = localStorage.getItem("schedule")
-var current = moment().format('MMMM Do YYYY, h:mm:ss a');
-var currenttime = moment().format('h:mm:ss a')
+
+
 
 
 
 //Functions
-  // To display the current day and time 
-    $("#currentDay").text(current)
+  
+  function updateday(){
+    // To display the current day and time 
+    var current = moment().format('MMMM Do YYYY, h:mm:ss a');
+    var currenttime = moment().format('HH');
+    var inputarea = document.querySelector(".hour")
+    console.log(currenttime)
+    $("#currentDay").html(current);
+
+    //To change the CSS as time goes on
+      //Present
+        if(currenttime == inputarea.id){
+          $(".form-control").addClass("present")
+        }
+      // Future
+        else if(currenttime > inputarea.id){
+          $(".form-control").addClass("future")
+        }
+      //Past
+        else{
+          $(".form-control").addClass("past")
+        };
+
+  }
+  setInterval(updateday,1000);
+
+
 
   // Function to save content into local storage 
     $(".saveBtn").on("click",function(e){
